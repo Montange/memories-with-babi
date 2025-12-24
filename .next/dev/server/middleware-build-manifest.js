8cf894fd0c9db1de176bcfd3774c033598f98235
Montange <richardkravenbiscante13@gmail.com>
2025-12-24 22:28:50 +0800

    new push

--- .next/dev/server/middleware-build-manifest.js
+++ .next/dev/server/middleware-build-manifest.js
@@ -3,9 +3,22 @@ globalThis.__BUILD_MANIFEST = {
     "/_app": []
   },
   "devFiles": [],
-  "polyfillFiles": [],
+  "polyfillFiles": [
+    "static/chunks/c04d5_next_dist_build_polyfills_polyfill-nomodule.js"
+  ],
   "lowPriorityFiles": [],
-  "rootMainFiles": []
+  "rootMainFiles": [
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_64d45882._.js",
+    "static/chunks/c04d5_next_dist_compiled_react-dom_55e341ed._.js",
+    "static/chunks/c04d5_next_dist_compiled_react-server-dom-turbopack_e0d9276a._.js",
+    "static/chunks/c04d5_next_dist_compiled_next-devtools_index_5126dc8a.js",
+    "static/chunks/c04d5_next_dist_compiled_a06aa9ba._.js",
+    "static/chunks/c04d5_next_dist_client_631c4840._.js",
+    "static/chunks/c04d5_next_dist_e8ea6a9c._.js",
+    "static/chunks/c04d5_@swc_helpers_cjs_57ea3b60._.js",
+    "static/chunks/Downloads_Album with Babi_memories-with-babi_a0ff3932._.js",
+    "static/chunks/turbopack-Downloads_Album with Babi_memories-with-babi_2529e12f._.js"
+  ]
 };
 globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
 "/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
